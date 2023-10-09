52c0ee558bfb5502283130edceac8b60b271f961
Marc Schultz <lunij@users.noreply.github.com>
2023-10-09 21:58:51 +0200

    Remove whitespace deprecations

--- Sources/StencilSwiftKit/StencilSwiftTemplate.swift
+++ Sources/StencilSwiftKit/StencilSwiftTemplate.swift
@@ -12,7 +12,6 @@ typealias NSRegularExpression = RegularExpression
 #endif
 
 // Workaround until Stencil fixes https://github.com/stencilproject/Stencil/issues/22
-@available(*, deprecated, message: "No longer needed with Stencil whitespace control features")
 open class StencilSwiftTemplate: Template {
   public required init(templateString: String, environment: Environment? = nil, name: String? = nil) {
     let templateStringWithMarkedNewlines = templateString
